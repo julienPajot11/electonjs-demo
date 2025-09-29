ba83c3461bbc1671224c686ced8f8e225aad1e9e
JulienPajot <julien.pajot@dupdevinci-edu.fr>
2025-09-29 09:37:03 +0200

    initial-commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <!--  Navigation-->
+  <ul uk-tab>
+    <li class="uk-active">
+      <router-link to="/">Page One</router-link>
+    </li>
+    <li>
+      <router-link to="/page-2">Page Two</router-link>
+    </li>
+  </ul>
+  <router-view />
+</template>
